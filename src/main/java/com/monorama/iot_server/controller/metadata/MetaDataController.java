f5e99b665ded8ed54f77280f819660d5ac0bc5cb
kimwiseong <kwws919@gmail.com>
2025-05-17 19:09:22 +0900

    :bug: Fix: MetaDataController 매핑 수정

--- src/main/java/com/monorama/iot_server/controller/metadata/MetaDataController.java
+++ src/main/java/com/monorama/iot_server/controller/metadata/MetaDataController.java
@@ -15,18 +15,18 @@ import org.springframework.web.bind.annotation.*;
 public class MetaDataController {
     private final MetaDataService metaDataService;
 
-    @GetMapping("{projectId}/item")
+    @GetMapping("/{projectId}/item")
     public ResponseDto<MetaDataItemListResponseDto> getMetaDataItemList(@PathVariable Long projectId) {
         return ResponseDto.ok(metaDataService.getMetaDataItemList(projectId));
     }
 
-    @PostMapping("{projectId}")
+    @PostMapping("/{projectId}")
     public ResponseDto<?> saveMetaData(@UserId Long userId, @PathVariable Long projectId, @RequestBody MetaDataListRequestDto metaDataList) {
         metaDataService.saveMetaData(userId, projectId, metaDataList);
         return ResponseDto.created(null);
     }
 
-    @GetMapping("{projectId}")
+    @GetMapping("/{projectId}")
     public ResponseDto<MetaDataListResponseDto> getMetaDataList(@PathVariable Long projectId) {
         return ResponseDto.ok(metaDataService.getMetaDataList(projectId));
     }
